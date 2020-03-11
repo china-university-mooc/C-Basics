2f03440848afc1566fba469bdafc7018cd8944ea
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-03-11 18:17:21 +0800

    code

--- C-Experiment/main.c
+++ C-Experiment/main.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <math.h>
-
-bool isPrime(int num) {
-    if (num < 2) {
-        return false;
-    }
-    
-    int max = sqrt(num);
-    for (int i = 2; i <= max; i++) {
-        if (num % i == 0) {
-            return false;
-        }
-    }
-    return true;
-}
 
 int main(int argc, const char * argv[]) {
-    int num;
-    scanf("%d", &num);
-
-    int p = 0;
-    int q = 0;
-    for (p = 2; p < num; p++) {
-        if (isPrime(p)) {
-            q = num - p;
-            if (isPrime(q)) {
-                break;
+    int m, n;
+    scanf("%d %d", &m, &n);
+    
+    
+    int i, j, s;
+    bool flag = false;
+    for (i = m; i <= n; i++) {
+        s = 1;
+        for (j = 2; j <= i/2; j++) {
+            if (i%j == 0) {
+                s = s + j;
             }
         }
+        if (s == i) {
+            flag = true;
+            printf("%d = 1", i);
+            for (j = 2; j <= i/2; j++) {
+                if (i%j == 0) {
+                    printf(" + %d", j);
+                }
+            }
+            printf("\n");
+        }
+    }
+    if (!flag) {
+        printf("None\n");
     }
     
-    printf("%d = %d + %d\n", num, p, q);
     return 0;
 }
