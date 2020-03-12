8eac7378f2210a70508b3f8a2fc9157672a2069e
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-03-12 17:06:16 +0800

    add code

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int a, n;
-    scanf("%d %d", &a, &n);
+    int flag, n;
+    double eps, item, s;
     
-    int sum = 0;
-    int item = 0;
-    for (int i = 1; i <= n; i++) {
-        item = item * 10 + a;
-        sum += item;
-    }
-    printf("s = %d\n", sum);
+    scanf("%lf", &eps);
+    flag = 1;
+    s = 0;
+    n = 1;
+    do {
+        item = 1.0 / n;
+        s = s + flag * item;
+        flag = - flag;
+        n = n + 3;
+    } while (item > eps);
+    
+    printf("sum = %f\n", s);
     
     return 0;
 }
