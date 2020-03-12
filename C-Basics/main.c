f12da6ed195dcbacc952822f6bbd8c7fa5bacee2
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-03-12 16:50:53 +0800

    add code

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -2,19 +2,22 @@
 #include <math.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    
-    int num, min;
+    int num;
     scanf("%d", &num);
-    min = num;
-    for (int i = 0; i < n - 1; i++) {
-        scanf("%d", &num);
-        if (min > num) {
-            min = num;
-        }
-    }
     
-    printf("min = %d\n", min);
+    if (num < 0) {
+        num = -1 * num;
+    }
+    int digit;
+    int sum = 0;
+    int count = 0;
+    do {
+        digit = num % 10;
+        sum += digit;
+        num /= 10;
+        count++;
+    } while(num > 0);
+    
+    printf("%d %d\n", count, sum);
     return 0;
 }
