293d3294864f600e8452dffe4550e1ebce99e028
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-03-12 17:11:18 +0800

    add code

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
 
 int main() {
-    double h;
-    int n;
-    scanf("%lf %d", &h, &n);
+    int num;
+    scanf("%d", &num);
     
-    double sum = -h;
-    double item = h;
-    if (n == 0) {
-        sum = 0;
-        item = 0;
-    }
-    for (int i = 0; i < n; i++) {
-        sum += 2*item;
-        item /= 2;
-    }
+    int a, b, c;
+    int min, mid, max;
+    int n1, n2;
+    int count = 1;
+    do {
+        a = num%10;
+        b = num/100;
+        c = (num - a - b*100)/10;
+        if (a > b) {
+            max = a;
+            min = b;
+        } else {
+            max = b;
+            min = a;
+        }
+        if (c > max) {
+            mid = max;
+            max = c;
+        } else if (c < min) {
+            mid = min;
+            min = c;
+        } else {
+            mid = c;
+        }
+        
+        n1 = max*100 + mid*10 + min;
+        n2 = min*100 + mid*10 + max;
+        num = n1 - n2;
+        printf("%d: %d - %d = %d\n", count, n1, n2, num);
+        count++;
+    } while (num != 495);
     
-    printf("%.1f %.1f\n", sum, item);
     return 0;
 }
