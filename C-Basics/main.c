6cbd3699419cdb6f5d48a573e6f53d481a1e7f67
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-03-18 16:23:12 +0800

    add code

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int sum = 0;
-    for(int i = 1; i <= 100; i++) {
-        sum = sum + i;
-    }
+    int n;
+    scanf("%d", &n);
     
-    printf("sum = %d\n", sum);
+    double sum = 0;
+    int sign = 1;
+    int denominator = 1;
+    for (int i = 0; i < n; i++) {
+        double item = sign * 1.0 / denominator;
+        sum += item;
+        sign = -sign;
+        denominator += 3;
+    }
+    printf("sum = %.3f\n", sum);
 
     return 0;
 }
