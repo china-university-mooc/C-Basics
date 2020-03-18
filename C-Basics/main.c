82376612f0b50be0b772a60c827b5a3816266e5d
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-03-18 17:10:16 +0800

    add code

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 
-double fact(int x);
-
 int main() {
-    int m, n;
-    scanf("%d %d", &m, &n);
+    int num;
+    scanf("%d", &num);
     
-    double c = fact(n) / fact(m) / fact(n - m);
-    printf("result = %.0f\n", c);
+    int a = num % 10;
+    num /= 10;
+    int b = num % 10;
+    num /= 10;
+    int c = num % 10;
+    
+    int out = a * 100 + b * 10 + c;
 
-    return 0;
+    printf("%d\n", out);
 }
-
-double fact(int x) {
-    double f = 1.0;
-    for (int i = 2; i <= x; i++) {
-        f *= i;
-    }
-    return f;
-}
-
