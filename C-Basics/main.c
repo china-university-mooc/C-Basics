fa7c5de8dd2b8a5295c311d382fe9327cd4871fa
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-03-18 16:17:53 +0800

    add code

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int x;
-    scanf("%d", &x);
-
-    double bill;
-    if (x < 0) {
-        printf("Invalid Value!\n");
-    } else if (x <= 50) {
-        bill = 0.53 * x;
-        printf("cost = %.2f\n", bill);
-    } else {
-        bill = 0.53 * x + 0.05 * (x - 50);
-        printf("cost = %.2f\n", bill);
+    int sum = 0;
+    for(int i = 1; i <= 100; i++) {
+        sum = sum + i;
     }
+    
+    printf("sum = %d\n", sum);
 
     return 0;
 }
