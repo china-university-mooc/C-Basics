fd31aad946cd84442b949eade20aed6749a201a6
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-03-18 15:29:57 +0800

    add code

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
+    int celsius, fahr;
+    fahr = 100;
+    celsius = 5 * (fahr-32) / 9;
+    printf("fahr=%d, celsius=%d\n", fahr, celsius);
     
-    for (int i = 1; i <= n; i++) {
-        if (i % 2 == 1) {
-            if (i != 1) {
-                printf(" ");
-            }
-            printf("%d", i);
-        }
-    }
-    printf("\n");
     return 0;
 }
