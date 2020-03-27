e453276829e227437b2ecbafa92adecf794cbff2
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-03-27 17:24:54 +0800

    add code

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -1,44 +1,23 @@
 #include <stdio.h>
-#include <math.h>
 
-int prime( int p );
-int PrimeSum( int m, int n );
+double fact(int n);
 
-int main()
-{
-    int m, n, p;
-
-    scanf("%d %d", &m, &n);
-    printf("Sum of ( ");
-    for( p=m; p<=n; p++ ) {
-        if( prime(p) != 0 )
-            printf("%d ", p);
+int main() {
+    int i;
+    double sum = 0;
+    
+    for (i = 1; i <= 10; i++) {
+        sum = sum + fact(i);
     }
-    printf(") = %d\n", PrimeSum(m, n));
+    printf("1!+2!+...+10!=%f\n", sum);
 
     return 0;
 }
 
-int prime( int p ) {
-    if (p < 2) {
-        return 0;
+double fact(int n) {
+    double result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
     }
-    
-    int limit = floor(sqrt(p));
-    for (int i = 2; i <= limit; i++) {
-        if (p % i == 0) {
-            return 0;
-        }
-    }
-    return 1;
-}
-
-int PrimeSum( int m, int n ) {
-    int sum = 0;
-    for (int i = m; i <= n; i++) {
-        if (prime(i)) {
-            sum += i;
-        }
-    }
-    return sum;
+    return result;
 }
