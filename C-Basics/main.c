6e1ad85e1af3d398b2a39c6ed59c7190bb336d1d
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-03-27 17:09:36 +0800

    add code

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
+double result_real, result_imag;
+void complex_prod(double x1, double y1, double x2, double y2);
 
 int main() {
-    int const limit = 200;
-    int a;
-    int b;
-    int decimal[limit];
+    double imag1, imag2, real1, real2;
+    scanf("%lf %lf", &real1, &imag1);
+    scanf("%lf %lf", &real2, &imag2);
     
-    scanf("%d/%d", &a, &b);
+    complex_prod(real1, imag1, real2, imag2);
+    
+    printf("product of complex is (%f)+(%f)i\n", result_real, result_imag);
+    
+    return 0;
+}
 
-    int cnt = 0;
-    int intCnt = 0;
-    while (cnt < limit && a > 0) {
-        if (a > b) {
-            intCnt++;
-        } else {
-            a = a * 10;
-        }
-        decimal[cnt++] = a / b;
-        a = a % b;
-    }
-    
-    if (intCnt == 0) {
-        printf("0");
-    }
-    for (int i = 0; i < cnt; i++) {
-        if (i == intCnt) {
-            printf(".");
-        }
-        printf("%d", decimal[i]);
-    }
-    printf("\n");
+void complex_prod(double x1, double y1, double x2, double y2)
+{
+    result_real = x1 * x2 - y1 * y2;
+    result_imag = x1 * y2 + x2 * y1;
 }
