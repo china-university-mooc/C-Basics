104ade34900f23f566e4c6ca4b874e5037493aa3
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-04-02 11:23:23 +0800

    add code

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -1,22 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int gcd( int x, int y );
+const int MAX = 101;
+void printPoly(int poly[], int len);
+void printItem(int c, int e);
 
 int main()
 {
-    int x, y;
+    int e, c;
+    int poly1[MAX] = {0};
+    int poly2[MAX] = {0};
+    int poly[MAX] = {0};
+    do {
+        scanf("%d %d", &e, &c);
+        poly1[e] = c;
+    } while (e > 0);
+    do {
+        scanf("%d %d", &e, &c);
+        poly2[e] = c;
+    } while (e > 0);
 
-    scanf("%d %d", &x, &y);
-    printf("%d\n", gcd(x, y));
+    for (int i = 0; i < MAX; i++) {
+        poly[i] = poly1[i] + poly2[i];
+    }
+    printPoly(poly, MAX);
 
     return 0;
 }
 
-int gcd( int x, int y ) {
-    while (y > 0) {
-        int temp = x % y;
-        x = y;
-        y = temp;
+void printPoly(int poly[], int len) {
+    int first = 1;
+    int hasItem = 0;
+    for (int i = len - 1; i >= 0; i--) {
+        if (poly[i] != 0) {
+            hasItem = 1;
+            
+            if (poly[i] < 0) {
+                printf("-");
+            } else {
+                if (!first) {
+                    printf("+");
+                }
+            }
+            
+            if (first) {
+                first = 0;
+            }
+            
+            printItem(poly[i], i);
+        }
     }
-    return x;
+    if (!hasItem) {
+        printf("0");
+    }
+    printf("\n");
+}
+
+void printItem(int c, int e) {
+    c = abs(c);
+    if (c == 0) {
+        printf("0");
+    } else if (c == 1 && e == 0) {
+        printf("1");
+    } else {
+        if (c > 1) {
+             printf("%d", c);
+        }
+        
+        if (e == 1) {
+            printf("x");
+        } else if (e > 1) {
+            printf("x%d", e);
+        }
+    }
 }
