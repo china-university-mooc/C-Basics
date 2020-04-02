0bbdd4bbfab7f5e1cd3243c6f02132b057d78da4
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-04-02 11:45:25 +0800

    add code

--- C-Basics/main.c
+++ C-Basics/main.c
@@ -1,76 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int MAX = 101;
-void printPoly(int poly[], int len);
-void printItem(int c, int e);
+int rowMax(int matrix[][100], int len, int r, int c);
+int colMin(int matrix[][100], int len, int r, int c);
 
 int main()
 {
-    int e, c;
-    int poly1[MAX] = {0};
-    int poly2[MAX] = {0};
-    int poly[MAX] = {0};
-    do {
-        scanf("%d %d", &e, &c);
-        poly1[e] = c;
-    } while (e > 0);
-    do {
-        scanf("%d %d", &e, &c);
-        poly2[e] = c;
-    } while (e > 0);
-
-    for (int i = 0; i < MAX; i++) {
-        poly[i] = poly1[i] + poly2[i];
+    int n;
+    scanf("%d", &n);
+    int matrix[100][100] = {0};
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
     }
-    printPoly(poly, MAX);
+    
+    int find = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (rowMax(matrix, n, i, j) && colMin(matrix, n, i, j)) {
+                printf("%d %d\n", i, j);
+                find = 1;
+            }
+        }
+    }
+    
+    if (!find) {
+        printf("NO\n");
+    }
 
     return 0;
 }
 
-void printPoly(int poly[], int len) {
-    int first = 1;
-    int hasItem = 0;
-    for (int i = len - 1; i >= 0; i--) {
-        if (poly[i] != 0) {
-            hasItem = 1;
-            
-            if (poly[i] < 0) {
-                printf("-");
-            } else {
-                if (!first) {
-                    printf("+");
-                }
-            }
-            
-            if (first) {
-                first = 0;
-            }
-            
-            printItem(poly[i], i);
+int rowMax(int matrix[][100], int len, int r, int c) {
+    int res = 1;
+    int max = matrix[r][c];
+    for (int i = 0; i < len; i++) {
+        if (matrix[r][i] > max) {
+            res = 0;
         }
     }
-    if (!hasItem) {
-        printf("0");
-    }
-    printf("\n");
+    return res;
 }
 
-void printItem(int c, int e) {
-    c = abs(c);
-    if (c == 0) {
-        printf("0");
-    } else if (c == 1 && e == 0) {
-        printf("1");
-    } else {
-        if (c > 1) {
-             printf("%d", c);
-        }
-        
-        if (e == 1) {
-            printf("x");
-        } else if (e > 1) {
-            printf("x%d", e);
+int colMin(int matrix[][100], int len, int r, int c) {
+    int res = 1;
+    int min = matrix[r][c];
+    for (int i = 0; i < len; i++) {
+        if (matrix[i][c] < min) {
+            res = 0;
         }
     }
+    return res;
 }
+
